8620cc0e4741b0339b8983802dbf07579dd5ae22
Abdul Majeed Kanoor <abdulkanoor@gmail.com>
2024-02-13 23:43:20 +0530

    Update Task and Validations
    Signed-off-by: Abdul Majeed Kanoor <abdulkanoor@gmail.com>

--- src/main/java/com/scaler/taskmanger/controllers/TaskController.java
+++ src/main/java/com/scaler/taskmanger/controllers/TaskController.java
@@ -1,12 +1,15 @@
 package com.scaler.taskmanger.controllers;
 
 import com.scaler.taskmanger.dto.CreateTaskDTO;
+import com.scaler.taskmanger.dto.ErrorResponseDTO;
+import com.scaler.taskmanger.dto.UpdateTaskDTO;
 import com.scaler.taskmanger.entities.TaskEntity;
 import com.scaler.taskmanger.service.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -34,9 +37,28 @@ public class TaskController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<TaskEntity> createTask(@RequestBody CreateTaskDTO  body) {
-       var task = taskService.addTask(body.getTitle(),body.getDescription() , body.getDeadline());
+    public ResponseEntity<TaskEntity> createTask(@RequestBody CreateTaskDTO body) throws ParseException {
+        var task = taskService.addTask(body.getTitle(), body.getDescription(), body.getDeadline());
         return ResponseEntity.ok(task); // Task created.
+    }
+
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskEntity> updateTask(@PathVariable("id") Integer id, @RequestBody UpdateTaskDTO body) throws ParseException {
+        var task = taskService.updateTask(id, body.getDescription(), body.getDeadline(), body.getCompleted());
+        if (task == null) {
+            return ResponseEntity.notFound().build(); // Task found.
+        }
+        return ResponseEntity.ok(task); // Task updated.
+    }
+
+    @ExceptionHandler(ParseException.class)
+    public ResponseEntity<ErrorResponseDTO> handleError(Exception e) {
+        if (e instanceof ParseException) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO("Invalid Date Format"));
+        }
+//        e.printStackTrace();
+        return ResponseEntity.internalServerError().body(new ErrorResponseDTO("Internal Server Error")); // Unknown error.
     }
 
 }
--- src/main/java/com/scaler/taskmanger/service/TaskService.java
+++ src/main/java/com/scaler/taskmanger/service/TaskService.java
@@ -14,13 +14,14 @@ public class TaskService {
     @Getter
     private ArrayList<TaskEntity> tasks = new ArrayList<>();
     private int taskId = 1;
+    private SimpleDateFormat deadlineFormatter = new SimpleDateFormat("yyyy-MM-dd");
 
-    public TaskEntity addTask(String title, String description, String deadline) {
+    public TaskEntity addTask(String title, String description, String deadline) throws ParseException {
         TaskEntity task = new TaskEntity();
         task.setId(taskId);
         task.setTitle(title);
         task.setDescription(description);
-        task.setDeadline(new Date());
+        task.setDeadline(deadlineFormatter.parse(deadline));
         task.setCompleted(false);
         tasks.add(task);
         taskId++;
@@ -35,6 +36,24 @@ public class TaskService {
             }
         }
         return null; // Task not found.
+    }
+
+
+    public TaskEntity updateTask(int id, String description, String deadline, Boolean completed) throws ParseException {
+        TaskEntity task = getTaskById(id);
+        if (task == null) {
+            return null; // Task not found.
+        }
+        if (description != null) {
+            task.setDescription(description);
+        }
+        if (deadline != null) {
+            task.setDeadline(deadlineFormatter.parse(deadline));
+        }
+        if (completed != null) {
+            task.setCompleted(true);
+        }
+        return task; // Task updated.
     }
 
 }
